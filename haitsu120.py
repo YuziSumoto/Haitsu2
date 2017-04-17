2e351a781e2bccabdb355bc6eb5301a21fa4c67e
YuziSumoto <yuzi@me.com>
2017-04-17 10:30:25 +0900

    明細シート出力機能追加

--- haitsu120.py
+++ haitsu120.py
@@ -55,19 +55,30 @@ class MainHandler(webapp2.RequestHandler):
     WorkSheet = WorkBook.add_sheet(u"請求一覧")  # シート追加
     self.SetPrintParam(WorkSheet) # 用紙サイズ等セット
 
+    WorkSheet2 = WorkBook.add_sheet(u"明細")  # シート追加
+    self.SetPrintParam(WorkSheet2) # 用紙サイズ等セット
+
     DataRec = DatSeikyu().GetMonth(Hizuke)
     Row = 0
     Col = 1
+
+    Row2 = 2
+
+    self.SetColSize2(WorkSheet2) # 明細シートサイズセット
+    self.SetTitle2(WorkSheet2,Style)      # 固定部分セット
+
     for Rec in DataRec: # データループ
       self.SetColSize(WorkSheet,Col) # 行,列サイズセット
       self.SetTitle(WorkSheet,Row,Col,Hizuke,Rec.Name,Style)      # 固定部分セット
-      self.SetData(WorkSheet,Rec,Row,Col,Style)      # データセット
+      self.SetData(WorkSheet,Rec,Row,Col,Style)    # データセット
+      self.SetData2(WorkSheet2,Rec,Row2,Style)      # 明細シートデータセット
       if Col == 1:
         Col = 5   # 右側
       else:
         Col = 1   # 左に戻す
         Row += 9  # 9行追加
-        
+      Row2 += 1 # 明細シート行
+
     return  WorkBook
   
   def SetPrintParam(self,WorkSheet): # 用紙サイズ・余白設定
@@ -80,6 +91,14 @@ class MainHandler(webapp2.RequestHandler):
     WorkSheet.header_str = ''
     WorkSheet.footer_str = ''
 #    WorkSheet.fit_num_pages = 1
+    return
+
+  def SetColSize2(self,WorkSheet):  # 行,列サイズセット
+
+    ColWidth = ["列の幅",3,3,10,8,8,8,6,6,8,6]
+    for i in range(1,len(ColWidth)):
+      WorkSheet.col(i - 1).width = int(ColWidth[i] * 400)
+
     return
 
   def SetColSize(self,WorkSheet,Col):  # 行,列サイズセット
@@ -120,6 +139,36 @@ class MainHandler(webapp2.RequestHandler):
 
     return
 
+  def SetTitle2(self,WorkSheet,Style):  # 固定部分セット
+
+    Style2 = self.SetStyle("THIN","THIN","THIN","THIN",xlwt.Alignment.VERT_CENTER,xlwt.Alignment.HORZ_CENTER) 
+    font = xlwt.Font() # Create the Font
+    font.height = 130
+    Style2.font = font
+
+    Col = 0
+    WorkSheet.write(1,Col,u"番号",Style2)
+    Col += 1
+    WorkSheet.write(1,Col,u"部屋\n番号",Style2)
+    Col += 1
+    WorkSheet.write(1,Col,u"氏名",Style2)
+    Col += 1
+    WorkSheet.write(1,Col,u"利用者負担額",Style2)
+    Col += 1
+    WorkSheet.write(1,Col,u"利用料自費請求額\n(家賃・食費など)",Style2)
+    Col += 1
+    WorkSheet.write(1,Col,u"ハイツⅡ計",Style2)
+    Col += 1
+    WorkSheet.write(1,Col,u"病院",Style2)
+    Col += 1
+    WorkSheet.write(1,Col,u"薬局",Style2)
+    Col += 1
+    WorkSheet.write(1,Col,u"合計",Style2)
+    Col += 1
+    WorkSheet.write(1,Col,u"領収日",Style2)
+
+    return
+
   def SetData(self,WorkSheet,Rec,Row,Col,Style):  # データ部分セット
 
     if Col == 1:
@@ -151,6 +200,65 @@ class MainHandler(webapp2.RequestHandler):
     WorkSheet.write(Row + 5,Col + 2,"",Style)
     WorkSheet.write(Row + 6,Col + 2,"",Style)
     WorkSheet.write(Row + 7,Col + 2,u"￥" + "{:,d}".format(int(Goukei)),Style)
+
+    return
+
+  def SetData2(self,WorkSheet,Rec,Row,Style):  # データ部分セット
+
+    Style2 = self.SetStyle("THIN","THIN","THIN","THIN",xlwt.Alignment.VERT_CENTER,xlwt.Alignment.HORZ_CENTER) 
+    font = xlwt.Font() # Create the Font
+    font.height = 130
+    Style2.font = font
+
+    StyleR = self.SetStyle("THIN","THIN","THIN","THIN",xlwt.Alignment.VERT_CENTER,xlwt.Alignment.HORZ_RIGHT) 
+    font = xlwt.Font() # Create the Font
+    font.height = 130
+    StyleR.font = font
+
+    WorkSheet.write(Row,0,Row - 1,Style2)
+    WorkSheet.write(Row,1,Rec.Room,Style2)
+    WorkSheet.write(Row,2,Rec.Name,Style2)
+
+    Goukei = 0
+    if Rec.Futan != None:
+      Goukei -= Rec.Futan
+      OutStr = u"￥" + "{:,d}".format(int(Rec.Futan))
+    else:
+      OutStr = ""
+    WorkSheet.write(Row,3,OutStr,StyleR)
+
+    if Rec.Haitu2Kei != None:
+      Goukei += Rec.Haitu2Kei
+      OutStr = u"￥" + "{:,d}".format(int(Rec.Haitu2Kei))
+    else:
+      OutStr = ""
+
+    WorkSheet.write(Row,4,u"￥" + "{:,d}".format(int(Goukei)),StyleR)
+
+    WorkSheet.write(Row,5,OutStr,StyleR)
+
+    if Rec.Byouin != None:
+      Goukei += Rec.Byouin
+      OutStr = u"￥" + "{:,d}".format(int(Rec.Byouin))
+    else:
+      OutStr = ""
+    WorkSheet.write(Row,6,OutStr,StyleR)
+
+    if Rec.Yakkyoku != None:
+      Goukei += Rec.Yakkyoku
+      OutStr = u"￥" + "{:,d}".format(int(Rec.Yakkyoku))
+    else:
+      OutStr = ""
+    WorkSheet.write(Row,7,OutStr,StyleR)
+
+    OutStr = u"￥" + "{:,d}".format(int(Goukei))
+    WorkSheet.write(Row,8,OutStr,StyleR)
+
+    if Rec.Ryosyubi != None:
+      OutStr = Rec.Ryosyubi.strftime('%Y/%m/%d')
+    else:
+      OutStr = ""
+    WorkSheet.write(Row,9,OutStr,StyleR)
 
     return
 
